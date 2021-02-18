c4a2b292300d1b0cf5fd65ffc829f97621cd660f
cutmeat <johannes.rave@gmail.com>
2021-02-18 20:49:13 +0100

    CountingValleys solution added

--- SalesByMatch/SalesByMatch.cs
+++ SalesByMatch/SalesByMatch.cs
@@ -1,12 +1,52 @@
 ﻿using System;
+using System.IO;
 
 namespace SalesByMatch
 {
-    class SalesByMatch
-    {
-        static void Main(string[] args)
+    class Solution {
+
+        // There is a large pile of socks that must be paired by color.
+        // Given an array of integers representing the color of each sock,
+        // determine how many pairs of socks with matching colors there are.
+        static int sockMerchant(int n, int[] socks)
         {
-            Console.WriteLine("Hello World!");
+            bool[] socksChecked = new bool[socks.Length];
+            int noOfPairs = 0;
+            
+            for (int i = 0; i < socks.Length; i++)
+            {
+                if (socksChecked[i]) continue;
+            
+                for (int j = i+1; j < socks.Length; j++)
+                {
+                    if (socksChecked[j]) continue;
+                    
+                    if (socks[i] == socks[j])
+                    {
+                        noOfPairs++;
+                        socksChecked[i] = true;
+                        socksChecked[j] = true;
+                        break;
+                    }
+                }
+            }
+
+            return noOfPairs;
+        }
+
+        static void Main(string[] args) {
+            TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            int[] ar = Array.ConvertAll(Console.ReadLine().Split(' '), arTemp => Convert.ToInt32(arTemp))
+                ;
+            int result = sockMerchant(n, ar);
+
+            textWriter.WriteLine(result);
+
+            textWriter.Flush();
+            textWriter.Close();
         }
     }
 }
