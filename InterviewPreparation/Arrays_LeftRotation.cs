9d74158a9e2f590324ff99966e84a6b632d99254
cutmeat <johannes.rave@gmail.com>
2021-02-20 21:37:58 +0100

    Arrays_LeftRotation solution added

---
+++ InterviewPreparation/Arrays_LeftRotation.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+
+namespace HackerRank_CSharp
+{
+    public class Arrays_LeftRotation
+    {
+
+        // Complete the rotLeft function below.
+        static int[] rotLeft(int[] a, int d)
+        {
+            int len = a.Length;
+            int offset = d % len;
+            
+            int[] res = new int[len];
+            Array.Copy(a, res, 0);
+            
+            for (int i = 0; i < len; i++)
+            {
+                int oldIndex = i + offset;
+                int newIndex = (oldIndex < len) ? oldIndex : oldIndex - len;
+                res[i] = a[newIndex];
+            }
+
+            return res;
+        }
+
+        public static void Main(string[] args)
+        {
+
+            int n = 4;
+
+            int d = 4;
+
+            int[] a = new int[] {1, 2, 3, 4, 5};
+
+            int[] result = rotLeft(a, d);
+
+            Console.WriteLine(String.Join(",", result));
+        }
+    }
+}
--- Run.cs
+++ Run.cs
@@ -10,7 +10,8 @@ namespace HackerRank_CSharp
             // Console.WriteLine(SockMerchant.sockMerchant(6, new []{10,10,30,10,50,50}));
             // Console.WriteLine(CountingValleys.countingValleys(0, "DDUUUUDDDDDDDUU"));
             // Day6_LetsReview.Main(null);
-            Arrays_2DArray.Main(null);
+            // Arrays_2DArray.Main(null);
+            Arrays_LeftRotation.Main(null);
         }
     }
 }
